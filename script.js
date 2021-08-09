cffaf009220534012bab47a8b470833c97b38bba
mapal62 <mapal62@gmail.com>
2021-08-09 16:03:13 +0200

    DOM append

--- script.js
+++ script.js
@@ -1,96 +1,124 @@
-let myVariable = false
+let myVariable = false;
 
-myVariable = true
+myVariable = true;
 if (myVariable === true) {
-    console.log('A feltétel igazra lett kiérétkelve.')
+    console.log("A feltétel igazra lett kiérétkelve.");
 }
 
 let a = 7;
-a = 2
+a = 2;
 if (a < 5) {
     console.log(a / 2);
 } else {
     console.log(a * 2);
 }
 
-for (let i = 0; i < 10; i=i+1) {
-    console.log(i)
+for (let i = 0; i < 10; i = i + 1) {
+    console.log(i);
 }
-for (let i = 1; i <= 10; i=i+1) {
-    console.log(i)
+for (let i = 1; i <= 10; i = i + 1) {
+    console.log(i);
 }
-for (let i=0; i<21; i++) {
-    if ((i % 2) === 0) {
-        console.log(i)
+for (let i = 0; i < 21; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
     }
 }
 //-----
-console.log('--Függvény: 1. feladat')
+console.log("--Függvény: 1. feladat");
 let printNumbersTill = (number) => {
-    for (let i=1; i<=number; i++) {
-        console.log(`Az első ${number}-ból a ${i}-ik.`)
+    for (let i = 1; i <= number; i++) {
+        console.log(`Az első ${number}-ból a ${i}-ik.`);
     }
-}
-printNumbersTill(10)
-printNumbersTill(15)
+};
+printNumbersTill(10);
+printNumbersTill(15);
 //-----
-console.log('--Függvény: 2. feladat')
-function getGreetingTo (name) {
-    return `Hello ${name}`
+console.log("--Függvény: 2. feladat");
+
+function getGreetingTo(name) {
+    return `Hello ${name}`;
 }
-getGreetingTo('Mark')
-console.log('most újra')
-console.log(getGreetingTo('Mark'))
+getGreetingTo("Mark");
+console.log("most újra");
+console.log(getGreetingTo("Mark"));
 //-----
-console.log('--Ciklus és tömb: 1. feladat')
-let printValues = [0,3,6,7,9]
+console.log("--Ciklus és tömb: 1. feladat");
+let printValues = [0, 3, 6, 7, 9];
+
 function arrayElement(element) {
-    console.log(element)
+    console.log(element);
 }
-printValues.forEach(arrayElement)
+printValues.forEach(arrayElement);
+
 function arrayList(anyArray) {
-    anyArray.forEach(console.log)
+    anyArray.forEach(console.log);
     for (let i = 0; i < anyArray.length; i++) {
-        console.log(anyArray[i])
+        console.log(anyArray[i]);
     }
     anyArray.forEach((xth) => {
-        console.log(xth)
-    })
+        console.log(xth);
+    });
 }
-arrayList(printValues)
+arrayList(printValues);
 let myNumbers = [10, 20, 30, 50, 12];
 
 // itt kijelentjük, hogy a tömb minden elemén szeretnénk valamit végrehajtani
 myNumbers.forEach((number) => {
-  // a függvényen belül adhatjuk meg a végrehajtandó műveleteket
-  console.log(number);
+    // a függvényen belül adhatjuk meg a végrehajtandó műveleteket
+    console.log(number);
 });
 //-----
-console.log('--Ciklus és tömb: 2. feladat - lásd fent')
+console.log("--Ciklus és tömb: 2. feladat - lásd fent");
 
 //DOM és jquery
- /* jQuery - 1. feladat */
-$('#fox-1').text('Macrotis')
-$('#fox-2').css('border-color','purple')
-$('#jq1 p').css('background-color','yellow')
+/* jQuery - 1. feladat */
+$("#fox-1").text("Macrotis");
+$("#fox-2").css("border-color", "purple");
+$("#jq1 p").css("background-color", "yellow");
 
- /* jQuery - 2. feladat */
- let color = "purple"
- let number ="10"
- let word ="cool"
+/* jQuery - 2. feladat */
+let color = "purple";
+let number = "10";
+let word = "cool";
 
- if (color === 'purple') {
-     $('#jq2 .north').css('background-color',color)
- }
+if (color === "purple") {
+    $("#jq2 .north").css("background-color", color);
+}
 
- if (number > 100) {
-     $('#jq2 .east').text('wow, de nagy szám')
- } else {
-    $('#jq2 .east').text('ez csak egy közönséges szám')
- }
+if (number > 100) {
+    $("#jq2 .east").text("wow, de nagy szám");
+} else {
+    $("#jq2 .east").text("ez csak egy közönséges szám");
+}
 
- if (word === 'cool') {
-     $('#jq2 .south').text('A DOM Ereje')
- } else {
-    $('#jq2 .south').text(word) 
- }
+if (word === "cool") {
+    $("#jq2 .south").text("A DOM Ereje");
+} else {
+    $("#jq2 .south").text(word);
+}
+
+/* jQuery DOM - 1. feladat */
+let jq3Nevek = ["Pali", "Ákos", "Gyula", "Aranka", "Zsuzsanna"];
+let myName = "Pali";
+jq3Nevek.forEach((nev) => {
+    if (nev === myName) {
+        $("#jq3 ul").append(`<li><strong>${nev}</strong></li>`);
+    } else {
+        $("#jq3 ul").append(`<li>${nev}</li>`);
+    }
+});
+
+function listaba(nev) {
+    $("#jq3 ul").append(`<li>${nev}</li>`);
+}
+
+listaba("Egerben");
+
+/* jQuery DOM - 2. feladat */
+let additionalBlock = {
+    title: "Beillesztve JavaScripttel",
+    text: "Ez a blokk a JavaScript jQuery könyvtárával lett beillesztve. Király!",
+};
+$('#jq3').append(`<h1>${additionalBlock.title}</h1>`)
+$('#jq3').append(`<p>${additionalBlock.text}</p>`)
